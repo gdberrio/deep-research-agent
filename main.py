e201381dbadde0bbb0abe010b741217b70e26b56
gdberrio <guilherme.diazberrio@gmail.com>
2026-03-29 14:51:45 +0100

    testing LLM api call

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from deepresearchagent!")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+endpoint = os.getenv("GPT-5-4-TARGET_URI")
+deployment_name = "gpt-5.4"
+api_key = os.getenv("GPT-5-4-API_KEY")
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=api_key
+)
+
+completion = client.chat.completions.create(
+    model=deployment_name,
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the capital of France?",
+        }
+    ],
+)
+
+print(completion.choices[0].message)
